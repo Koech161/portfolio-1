ce4b91635bae27a7c17fa8dd8ef2e9537faf08c3
Abel Koech <koechabel161@gmail.com>
2024-08-22 02:34:50 +0300

    add cv

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -12,7 +12,7 @@ const Resume = () => {
         <div className='resume'>
             <h1>My Resume</h1>
         <h3>Discover more about my skills and background by downloading my resume.</h3>
-      <button type='primary'>   <FaFileDownload size={24}/> Download CV</button>
+      <button type='primary'>   <FaFileDownload size={24}/><a href='https://docs.google.com/document/d/1i6FfZ1cJL0k6BkmwjGJaKLauqkB76VgWRSMvwY5lawQ/edit#heading=h.5rf9wr4r3no2' target='_blank' rel='noopener noreferrer' >Download CV</a></button>
         </div>
         <h2 className='title'> <FaCogs size={28}/>  My Skills</h2>
         <Skills/>

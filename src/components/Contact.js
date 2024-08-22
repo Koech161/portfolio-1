35a505006679c672bae4b97b9a21eeb2ab55ecc5
Abel Koech <koechabel161@gmail.com>
2024-08-22 12:58:01 +0300

    add form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaGithub, FaLinkedin} from 'react-icons/fa'
+import { FaEnvelopeOpenText, FaGithub, FaLinkedin} from 'react-icons/fa'
 import NavBar from './NavBar'
 import '../style/Contact.css'
 
@@ -18,6 +18,19 @@ const Contact = () => {
           <FaGithub size={24} color="#333"/>
           <p><a href='https://github.com/koech161' target='_blank' rel='noopener noreferrer'>GitHub </a></p>
         </div>
+        <div className='contact-item'>
+          <h3><FaEnvelopeOpenText/> Email</h3>
+        </div>
+        
+        <>
+        <form action='' method='POST' className='form' >
+            <label>Email</label>
+            <input type='email'  placeholder='enter your email' name='email'/>
+            <label>message</label>
+            <textarea  placeholder='enter your message'/>
+            <input type='submit' />
+          </form>
+        </>
       
     </div>
   

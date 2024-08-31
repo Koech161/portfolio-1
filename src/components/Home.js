ad0474c241418dba9790fceb7aa32be73599eb2e
Abel Koech <koechabel161@gmail.com>
2024-08-31 14:29:40 +0300

    troubleshooting

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,18 +5,7 @@ import '../App.css'
 import NavBar from './NavBar'
 
 const Home = () => {
- 
-const [sentenceIndex, setSentenceIndex] =useState(0)
- const sentences =[
-  'Front-end Developer',
-  'React Developer'
- ]
-  useEffect(()=>{
-    const interval = setInterval(() => {
-      setSentenceIndex(prev=> (prev + 1) % sentences.length)
-    },2500);
-   return () =>clearInterval(interval)
-  },[])
+
   return (
 
     <div className='home'>
@@ -29,7 +18,7 @@ const [sentenceIndex, setSentenceIndex] =useState(0)
       <div className='bio '>
       <h2>Hello, I'm</h2>
         <h3>Abel Koech</h3>
-        <h3> a <span className='span'>{sentences[sentenceIndex]}</span></h3>
+        <h3> a <span className='span'>Front-end Developer</span></h3>
       </div>
     <div className='home-img mb-3'>
         <img src={homeimg} alt='Abel Koech'/>

cc0a865887b59540c1316ed64d176faf81648542
Abel Koech <koechabel161@gmail.com>
2024-09-04 16:47:59 +0300

    optimize

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 import routes from './routes';
 import Footer from './components/Footer'
 import './style/Footer.css'
-import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className='App'>
       
       <div className='content'>
-      {/* <NavBar/> */}
+    
       <RouterProvider router={routes}/>
       </div>
      
